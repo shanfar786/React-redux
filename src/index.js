fe6d4006a7082a412364a72b0c1177e79da43a2d
shanfar786 <Shanfar786@gmail.com>
2021-07-13 23:40:31 +0530

    new

--- src/index.js
+++ src/index.js
@@ -2,50 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { store } from './app/store';
 import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
-import { createStore } from 'redux'; 
-// import ReactDOM from 'react-dom';
-
-
-//selector
-
-const SHANFAR_NAME = 'SHANFAR_NAME';
- const MUNNAR_PLACE = 'MUNNAR_PLACE';
-//Action
-const mynameShanfarAction = () => ({type : SHANFAR_NAME})
- const mynameNatureAction = () => ({type : MUNNAR_PLACE})
-
-//Reducer
-const shanfarReducer = (state,actions) => {
-  switch (actions.type){
-    case 'SHANFAR_NAME':
-      return state = "my name shanfar"
-     case 'MUNNAR_PLACE':
-     return state = "i love India"
-    default :
-    return state = "no selected"
-    
-  }
-}
-//
-
-let store = createStore(shanfarReducer);
-
-store.subscribe(() => {console.log(store.getState())});
-
-store.dispatch(mynameShanfarAction());
-store.dispatch(mynameNatureAction());
-
-ReactDOM.render(<Provider store ={store}>
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode></Provider>,
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
