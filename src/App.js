b8737c87304223406f9b188ab24668ef921e4b10
shanfar786 <Shanfar786@gmail.com>
2021-07-14 01:54:55 +0530

    img

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Case from './Components/Case'
 
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <Case />
        
       </header>
